d713a1a081a5f7709eb30c43fc3865b1af5fc186
HyeSeong <hyeseong43@gmail.com>
2024-05-27 08:26:05 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.07 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,36 +1,29 @@
 class Solution {
-    public static int solution(String my_string) {
-        int answer = 0;
+    public static int solution(String myString) {
+        int sum = 0;
+        StringBuilder number = new StringBuilder();
 
-        int i = 0;
-        while (i < my_string.length()) {
-            StringBuilder sb = new StringBuilder();
-            int idx = i;
-
-            while ( idx < my_string.length() && Character.isDigit(my_string.charAt(idx)) ){
-                sb.append(my_string.charAt(idx++));
-
+        for (int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            if (Character.isDigit(ch)) {
+                number.append(ch);
+            } else {
+                sum += parseIntOrZero(number.toString());
+                number.setLength(0); // StringBuilder 비우기
             }
-            String checkDigit = sb.toString();
-            if(isInteger(checkDigit)){
-                answer += Integer.parseInt(checkDigit);
-                if(checkDigit.length() != 1){
-                    i += checkDigit.length() - 1;
-                }
-            }
-
-            i++;
         }
 
-        return answer;
+        // 마지막 숫자 처리
+        sum += parseIntOrZero(number.toString());
+
+        return sum;
     }
 
-    private static boolean isInteger(String str){
-        try{
-            Integer.parseInt(str);
-            return true;
-        }catch(NumberFormatException e){
-            return false;
+    private static int parseIntOrZero(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return 0;
         }
     }
 }
