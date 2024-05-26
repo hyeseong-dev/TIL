a9926fab86ae21947acffea38758c52177a5b999
HyeSeong <hyeseong43@gmail.com>
2024-05-27 06:51:08 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.41 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,11 +1,9 @@
+import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i=0; i<my_string.length(); i++){
-            if(!answer.contains(String.valueOf(my_string.charAt(i)))){
-                answer += my_string.charAt(i);
-            }
-        }
-        return answer;
+        String[] answer = my_string.split("");
+        Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
+
+        return String.join("", set);
     }
 }
