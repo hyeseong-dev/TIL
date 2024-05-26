dcc7b7e4bdc14db02e966ce669027c1df3adba1d
HyeSeong <hyeseong43@gmail.com>
2024-05-26 22:47:18 +0900

    [level 0] Title: 숫자 찾기, Time: 0.22 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int num, int k) {
-        String s = String.valueOf(num);
+        String[] numArr = String.valueOf(num).split("");
 
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) - '0' == k){
+        for (int i = 0;i < numArr.length; i++) {
+            if (String.valueOf(k).equals(numArr[i])) {
                 return i + 1;
             }
         }
+
         return -1;
     }
 }
