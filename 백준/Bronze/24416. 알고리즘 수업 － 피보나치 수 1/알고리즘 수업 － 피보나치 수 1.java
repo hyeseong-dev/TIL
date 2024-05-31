d274794b7ce81ddadbec598bc98a795b6ec269f4
HyeSeong <hyeseong43@gmail.com>
2024-05-31 17:31:11 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 160 ms, Memory: 16072 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -8,21 +8,31 @@ public class Main {
 //
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); //Int
-        int resultRecursive = fib(n);
+        int n = Integer.parseInt(br.readLine()); 
+//        int resultRecursive = fib(n);
 //        int resultDP = fibonacci(n);
 
-        System.out.println(recursiveCount + " " + (n-2)); // 코드1, 코드2 실행 횟수 출력
+        System.out.println(fibonacci(n) + " " + (n-2)); // 코드1, 코드2 실행 횟수 출력
     }
 
-    public static int fib(int n) {
-        if (n == 1 || n == 2) {
-            recursiveCount++; // 코드1 실행
-            return 1;
-        } else {
-            return fib(n - 1) + fib(n - 2);
+    // 동적 프로그래밍을 이용하여 피보나치 수를 계산
+    static int fibonacci(int N){
+        int[] f = new int[N + 1];
+        f[1] = f[2] = 1;
+        for(int i = 3; i <= N; i++) { // 반복문을 통해 이전 값들을 이용하여 피보나치 수를 계산
+            f[i] = f[i - 1] + f[i - 2];
         }
+        return f[N]; // 계산된 피보나치 수
     }
+
+//    public static int fib(int n) {
+//        if (n == 1 || n == 2) {
+//            recursiveCount++; // 코드1 실행
+//            return 1;
+//        } else {
+//            return fib(n - 1) + fib(n - 2);
+//        }
+//    }
 
 //    public static int fibonacci(int n) {
 //        int[] f = new int[n + 1];
