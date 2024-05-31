8af74025ce99ab76f021d66f2ed78f091ee74990
HyeSeong <hyeseong43@gmail.com>
2024-05-31 18:56:34 +0900

    [Silver III] Title: N과 M (2), Time: 192 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -7,31 +7,12 @@ public class Main {
         int M = scanner.nextInt();
         scanner.close();
         
-        generateCombinations(N, M);
-    }
-
-    public static void generateCombinations(int N, int M) {
-        // DP table to store combinations
-        int[][] dp = new int[N + 1][M + 1];
-
-        // Initialize the DP table
-        for (int i = 0; i <= N; i++) {
-            for (int j = 0; j <= Math.min(i, M); j++) {
-                if (j == 0 || j == i) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                }
-            }
-        }
-
-        // Array to store the current combination
         int[] combination = new int[M];
-        printCombinations(dp, N, M, 0, 1, combination);
+        generateCombinations(N, M, 1, 0, combination);
     }
 
-    public static void printCombinations(int[][] dp, int N, int M, int index, int start, int[] combination) {
-        if (index == M) {
+    public static void generateCombinations(int N, int M, int start, int depth, int[] combination) {
+        if (depth == M) {
             for (int i = 0; i < M; i++) {
                 System.out.print(combination[i] + " ");
             }
@@ -40,10 +21,8 @@ public class Main {
         }
 
         for (int i = start; i <= N; i++) {
-            if (dp[N - i][M - index - 1] > 0) {
-                combination[index] = i;
-                printCombinations(dp, N, M, index + 1, i + 1, combination);
-            }
+            combination[depth] = i;
+            generateCombinations(N, M, i + 1, depth + 1, combination);
         }
     }
 }
