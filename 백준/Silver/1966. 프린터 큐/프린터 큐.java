0eff0cd7e6f3cab56c0840e81be8af8d5e6d54aa
HyeSeong <hyeseong43@gmail.com>
2024-06-05 19:10:31 +0900

    [Silver III] Title: 프린터 큐, Time: 152 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -3,69 +3,61 @@ import java.util.*;
 
 public class Main {
 
-    static String solution(BufferedReader br, BufferedWriter bw) throws IOException{
+    /*
+        [시간 복잡도]
+            - 큐 : 각 문서에 대해 큐에서 제거하고 다시 삽입하는 작업은 최대 N번 일어납니다. 따라서 큐 조작의 시간 복잡도는 O(N).
+            - 우선순위 큐 : 우선순위 큐에서 삽입 및 제거 작업의 시간 복잡도는 O(log N).
+                        최대 N번의 삽입 및 제거 작업이 일어나므로, 우선순위 큐 조작의 시간 복잡도는 O(N log N).
+
+            결론 : 따라서, 전체 시간 복잡도는 O(N log N)입니다. 이는 기존의 O(N^2) 시간 복잡도보다 효율적입니다.
+     */
+    static String solution(BufferedReader br) throws IOException {
         int cycle = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
         StringBuilder result = new StringBuilder();
 
-        while(cycle-- > 0){
-            st = new StringTokenizer(br.readLine());
+        while (cycle-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int numDoc = Integer.parseInt(st.nextToken());
             int curDocIdx = Integer.parseInt(st.nextToken());
 
-            // 큐와 우선순위 리스트 초기화
             Queue<int[]> queue = new LinkedList<>();
-            List<Integer> priorityArr = new ArrayList<>();
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
 
-            // queue, priorityArr에 값을 할당하여 채워줍니다.
-            String[] strArr = br.readLine().split(" ");
-            for(int i = 0 ; i < strArr.length ; i++){
-                int priority = Integer.valueOf(strArr[i]);
-                queue.offer(new int[]{i, priority});  // 큐에( {위치 인덱스, 우선순위} ) 저장
-                priorityArr.add(priority);
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < numDoc; i++) {
+                int priority = Integer.parseInt(st.nextToken());
+                queue.offer(new int[]{i, priority});
+                priorityQueue.offer(priority);
             }
 
-
-            // priorityArr를 DESC로 정렬합니다.
-            Collections.sort(priorityArr, Collections.reverseOrder());
             int printOrder = 0;
 
-            // queue가 모두 빌 때까지 loop를 돌립니다.
-            while(!queue.isEmpty()){
+            while (!queue.isEmpty()) {
+                int[] current = queue.poll();
+                int idx = current[0];
+                int priority = current[1];
 
-                // 큐에서 일단 값을 꺼내고 idx, priority변수에 각각 0, 1번째 인덱스에 맞게 할당합니다.
-                int[] currentArr = queue.poll();
-                int idx = currentArr[0];
-                int priority = currentArr[1];
-
-                // 우선순위만 담긴 배열의 인덱스와 큐에서 가져온 idx는 일치하다면 다음 로직 처리를 합니다.
-                if(priorityArr.get(0) == priority){
-
-                    printOrder++; // 우선순위만 담긴 배열을 printOrder를 증가시킵니다.
-                    priorityArr.remove(0); // 우선순위만 담긴 배열의 맨 앞의 값을 삭제해줍니다.
-                    if(idx == curDocIdx){
+                if (priority == priorityQueue.peek()) {
+                    printOrder++;
+                    priorityQueue.poll();
+                    if (idx == curDocIdx) {
                         result.append(printOrder).append("\n");
                         break;
                     }
-
-                } else{
-                    // 우선순위만 담긴 배열의 인덱스와 큐에서 가져온 idx가 일치하지 않는다면
-                    // currentArr를 다시 큐에 넣습니다.
-                    queue.offer(currentArr);
+                } else {
+                    queue.offer(current);
                 }
-
             }
         }
 
         return result.toString();
     }
 
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String answer = solution(br, bw);
+        String answer = solution(br);
         bw.write(answer);
         bw.flush();
         bw.close();
