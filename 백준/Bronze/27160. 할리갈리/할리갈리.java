0156bf7168943bc22ba7730766c98f0741d22c1d
HyeSeong <hyeseong43@gmail.com>
2024-06-06 10:33:52 +0900

    [Bronze II] Title: 할리갈리, Time: 496 ms, Memory: 49360 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.java
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -1,13 +1,16 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
 
-    public static Map<String, Integer> fruitFactory(){
+    public static Map<String, Integer> fruitFactory() {
         Map<String, Integer> fruits = new HashMap<>();
         fruits.put("BANANA", 0);
         fruits.put("PLUM", 0);
@@ -16,16 +19,17 @@ public class Main {
         return fruits;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String answer = "NO";
-        Scanner scanner = new Scanner(System.in);
-        int scNum = scanner.nextInt();
-        scanner.nextLine(); // 개행 문자 처리
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int scNum = Integer.parseInt(reader.readLine());
 
         Map<String, Integer> fruits = fruitFactory();
 
         for (int i = 1; i <= scNum; i++) {
-            String strRaw = scanner.nextLine();
+            String strRaw = reader.readLine();
             String[] strList = strRaw.split(" ");
             String fruitName = strList[0];
             int fruitNum = Integer.parseInt(strList[1]);
@@ -38,7 +42,11 @@ public class Main {
             answer = "YES";
         }
 
-        System.out.println(answer);
-        scanner.close();
+        writer.write(answer);
+        writer.newLine();
+        writer.flush();
+
+        reader.close();
+        writer.close();
     }
 }
