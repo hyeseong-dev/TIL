05dc55961f52a9e36d1d342b04fa4cb9c4274a2a
HyeSeong <hyeseong43@gmail.com>
2024-06-06 10:08:17 +0900

    [Bronze I] Title: 민균이의 비밀번호, Time: 136 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
+++ 백준/Bronze/9933. 민균이의 비밀번호/민균이의 비밀번호.java
@@ -1,58 +1,51 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
 
+// 기존 Main 클래스의 코드의 중복성을 줄이고 재사용성을 높이고 가독성 향상에 초점
 public class Main {
 
     public static String solution(BufferedReader br) throws IOException {
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int cycles = Integer.parseInt(st.nextToken());
+        int cycles = Integer.parseInt(br.readLine());
+        Set<String> stringSet = new HashSet<>();
 
-        String[] stringArr = new String[cycles];
-        // cycle만큼 loop를 돌며 문자열 데이터를 버퍼에서 가져옵니다.
+        // cycles 만큼 loop를 돌며 문자열 데이터를 버퍼에서 가져옵니다.
         for (int i = 0; i < cycles; i++) {
             String string = br.readLine();
-            int loopNum =  string.length() / 2;
+            stringSet.add(string);
 
-            boolean isPalindrome = true;
-            // 버퍼에서 받아온 한줄의 문자를 팰린드롬 알고리즘의 투포인터로 확인처리함.
-            for (int leftPointer = 0, rightPointer=string.length()-1;
-                 leftPointer < loopNum;
-                 leftPointer++, rightPointer--
-            ){
-
-                // 문자, 0번째 인덱스 <-> (마지막 인덱스) 값 일치여부 비교
-                // 문자, 1번째 인덱스 <-> (마지막 인덱스 -1) 값 일치여부 비교
-                // ... leftPointer 증가하고 rightPointer는 감소함.
-                if(string.charAt(leftPointer) != string.charAt(rightPointer)){
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            if(isPalindrome){
-                return String.format("%d %s", string.length(), string.charAt(loopNum));
-            }else{
-                stringArr[i] = string;
+            // 팰린드롬인지 확인
+            if (isPalindrome(string)) {
+                return String.format("%d %s", string.length(), string.charAt(string.length() / 2));
             }
         }
 
-        // 특정 문자열(reverse 처리한)이 문자열 배열에 존재하는지 확인
-        for(int i = 0; i < cycles; i++){
-            String reversedString = new StringBuilder(stringArr[i]).reverse().toString();
-            if(Arrays.asList(stringArr).contains(reversedString)){
-                int midIdx = reversedString.length() /2;
-                return String.format("%d %s", reversedString.length(), reversedString.charAt(midIdx));
+        // 특정 문자열(reverse 처리한)이 문자열 집합에 존재하는지 확인
+        for (String string : stringSet) {
+            String reversedString = new StringBuilder(string).reverse().toString();
+            if (stringSet.contains(reversedString)) {
+                return String.format("%d %s", reversedString.length(), reversedString.charAt(reversedString.length() / 2));
             }
         }
 
         return "";
-
     }
 
-    public static void main(String[] args) throws IOException {
+    // 팰린드롬 여부를 확인하는 메서드
+    private static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
         // try-with-resources 문을 사용하여 자원을 자동으로 닫도록 처리
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
@@ -63,6 +56,5 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
