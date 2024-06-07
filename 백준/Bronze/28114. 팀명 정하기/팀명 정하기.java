a03b44b7ad335caa871359e10e3b330296330b21
HyeSeong <hyeseong43@gmail.com>
2024-06-07 18:09:46 +0900

    [Bronze II] Title: 팀명 정하기, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.java
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,46 +8,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] person1 = br.readLine().split(" ");
-        String[] person2 = br.readLine().split(" ");;
-        String[] person3 = br.readLine().split(" ");;
+        String[][] persons = new String[3][3];
 
-        int p1 = Integer.parseInt(person1[0]);
-        int p2 = Integer.parseInt(person2[0]);
-        int p3 = Integer.parseInt(person3[0]);
-
-        int y1 = Integer.parseInt(person1[1]);;
-        int y2 = Integer.parseInt(person2[1]);;
-        int y3 = Integer.parseInt(person3[1]);;
-
-        String s1 = person1[2];
-        String s2 = person2[2];
-        String s3 = person3[2];
+        for (int i = 0; i < 3; i++) {
+            persons[i] = br.readLine().split(" ");
+        }
 
         int[][] scoreArr = new int[3][2];
-        scoreArr[0][0] = 0;
-        scoreArr[0][1] = p1;
-        scoreArr[1][0] = 1;
-        scoreArr[1][1] = p2;
-        scoreArr[2][0] = 2;
-        scoreArr[2][1] = p3;
+        int[] yearArr = new int[3];
+        String[] surArr = new String[3];
 
-
-        int[] yearArr = new int[]{y1, y2, y3};
-        String[] surArr = new String[]{s1, s2, s3};
-
+        for (int i = 0; i < 3; i++) {
+            scoreArr[i][0] = i;
+            scoreArr[i][1] = Integer.parseInt(persons[i][0]);
+            yearArr[i] = Integer.parseInt(persons[i][1]);
+            surArr[i] = persons[i][2];
+        }
 
         Arrays.sort(yearArr);
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < yearArr.length; i++) {
-             String subStrYear = String.valueOf(yearArr[i]).substring(2);
-             sb.append(subStrYear);
+        for (int year : yearArr) {
+            String subStrYear = String.valueOf(year).substring(2);
+            sb.append(subStrYear);
         }
-
         System.out.println(sb.toString());
 
         StringBuilder sb2 = new StringBuilder();
-
         Arrays.sort(scoreArr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -56,15 +41,12 @@ public class Main {
             }
         });
 
-        for(int i = 0; i < scoreArr.length; i++) {
+        for (int i = 0; i < scoreArr.length; i++) {
             int idx = scoreArr[i][0];
             String subStrSur = String.valueOf(surArr[idx].charAt(0));
             sb2.append(subStrSur);
         }
         System.out.println(sb2.toString());
         br.close();
-
     }
-
-
 }
