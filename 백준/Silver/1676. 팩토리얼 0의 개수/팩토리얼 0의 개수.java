6a67bc98b106a0a82bcc53d7339c3015dde0a4e3
HyeSeong <hyeseong43@gmail.com>
2024-08-05 19:13:24 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(countTrailingZeros(n));
+    }
+
+    public static int countTrailingZeros(int n) {
+        int count = 0;
+        for (int i = 5; i <= n; i *= 5) {
+            count += n / i;
+        }
+        return count;
+    }
+}
