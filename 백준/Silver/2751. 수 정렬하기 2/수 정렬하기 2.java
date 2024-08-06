76a598aa62b88bfc0842472c3cf6dde9d117bf93
HyeSeong <hyeseong43@gmail.com>
2024-08-06 21:39:13 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1784 ms, Memory: 148948 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,28 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
 
+/**
+ * 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            int N = Integer.parseInt(br.readLine());
-            int[] intArr = new int[N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-            for (int i = 0; i < N; i++) {
-                intArr[i] = Integer.parseInt(br.readLine());
-            }
-            Arrays.sort(intArr);
-            for(int i=0; i< N; i++){
-                System.out.println(intArr[i]);
-            }
-        }
+		Set<Integer> set = new TreeSet<>();
+		for (int i = 0; i < n; i++) {
+			set.add(Integer.parseInt(br.readLine()));
+		}
 
-    }
+		StringBuilder sb = new StringBuilder();
+		for (int result : set) {
+			sb.append(result).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
 }
